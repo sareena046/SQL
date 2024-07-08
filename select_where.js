43eea53dee8df6e9efe829e8a3cbef0b01dfc55b
sareena046 <157474223+sareena046@users.noreply.github.com>
2024-07-08 21:31:28 +0700

    Update select_where.js

--- select_where.js
+++ select_where.js
@@ -11,7 +11,7 @@ con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
 
-    var sql = "SELECT * FROM Customers WHERE address = 'Songkala'";
+    var sql = "SELECT * FROM Customers WHERE address = 'Songkhla'";
 
     con.query(sql, function (err, result, fields) {
         if (err) {
